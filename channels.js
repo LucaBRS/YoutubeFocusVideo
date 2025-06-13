f2014e94073d477080172a8ac6422a8d805bbd4f
luca <luca.barsottini@motusml.com>
2025-06-13 11:09:28 +0200

    add tampermonkey file and channels

---
+++ channels.js
@@ -0,0 +1,19 @@
+window.allowedChannels = [
+    'freeCodeCamp.org',
+    'Fireship',
+    'Traversy Media',
+    'The Net Ninja',
+    'Tech With Tim',
+    'Lex Fridman',
+    'TechWorld with Nana',
+    'Ben Felix',
+    'The Diary Of A CEO'
+];
+
+window.allowedKeywords = [
+    'Python',
+    'Docker',
+    'Kubernetes',
+    'Tutorial',
+    'DevOps'
+];
